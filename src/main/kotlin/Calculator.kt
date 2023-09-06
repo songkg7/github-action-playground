8d2a087384ed4ca31162d3885528637818dea28b
haril song <songkg7@gmail.com>
2023-09-06 21:28:32 +0900

    ci: technical commit

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,14 +1,8 @@
 class Calculator {
 
-    fun sum(
-        a: Int,
-        b: Int,
-    ) = a + b
+    fun sum(a: Int, b: Int) = a + b
 
-    fun subtract(
-        a: Int,
-        b: Int,
-    ) = a - b
+    fun subtract(a: Int, b: Int) = a - b
 
     fun multiply(
         a: Int,
