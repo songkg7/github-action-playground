ac39614f87bcc0deef0aa76de62930d11e1c5e05
Haril Song <songkg7@gmail.com>
2023-09-06 17:25:45 +0900

    ci: technical commit (#3)
    * ci: technical commit

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,7 +1,22 @@
 class Calculator {
 
-    fun sum(a: Int, b: Int) = a + b
+    fun sum(
+        a: Int,
+        b: Int,
+    ) = a + b
 
-    fun subtract(a: Int, b: Int) = a - b
+    fun subtract(
+        a: Int,
+        b: Int,
+    ) = a - b
 
+    fun multiply(
+        a: Int,
+        b: Int,
+    ) = a * b
+
+    fun divide(
+        a: Int,
+        b: Int,
+    ) = a / b
 }
--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -13,4 +13,13 @@ class CalculatorTest : FreeSpec({
         calc.subtract(1, 1) shouldBe 0
     }
 
+    "2 * 3 = 6" {
+        val calc = Calculator()
+        calc.multiply(2, 3) shouldBe 6
+    }
+
+    "6 / 3 = 2" {
+        val calc = Calculator()
+        calc.divide(6, 3) shouldBe 2
+    }
 })
