1fc9f17f1591b2ac663197fd9d076acfe423764b
kgsong <kyunggeun.song@seavantage.com>
2023-09-06 13:37:06 +0900

    ci: technical commit

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -7,11 +7,11 @@ class Calculator {
 
     fun subtract(
         a: Int,
-        b: Int
+        b: Int,
     ) = a - b
 
     fun multiply(
         a: Int,
-        b: Int
+        b: Int,
     ) = a * b
 }
